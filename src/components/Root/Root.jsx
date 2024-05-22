d29bbeecb0a060598201d5ec1780602e4591e56f
nahar-nur <naharbb@gmail.com>
2024-05-22 17:01:38 +0600

    project setup with react router, tailwind and daisy ui

---
+++ src/components/Root/Root.jsx
@@ -0,0 +1,14 @@
+import { Outlet } from "react-router-dom";
+import Home from "../Home/Home";
+
+
+const Root = () => {
+    return (
+        <div>
+            <Home></Home>
+           <Outlet></Outlet> 
+        </div>
+    );
+};
+
+export default Root;
